ef07956a537559c4f324a263d4c917cba1e714dc
Spati Dube <sdube@appdev.yearup.org>
2025-04-28 14:00:08 -0500

    initial commit

---
+++ AccountingLegder/src/main/java/main.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+
+public class main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        while (true){
+            System.out.println(" \n -Accounting Ledger-");
+            Stystem.ou.println("D ) Add Deposits");
+
+        }
+    }
+}
